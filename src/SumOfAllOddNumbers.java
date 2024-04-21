584d940dce7377cc5ec105a1c5755d7645c97e22
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-04-22 00:06:37 +0530

    My Second Commit

---
+++ src/SumOfAllOddNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfAllOddNumbers {
+    public static void main(String[] args){
+        int sum=0;
+        for (int i=0;i<=100;i++){
+            if (i%2!=0){
+                sum=sum+i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
