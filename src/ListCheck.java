c698b4bd7aa3156d141880ac84b7f353e6c0f546
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-04-28 03:04:42 +0530

    My First Commit

---
+++ src/ListCheck.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.util.Arrays;
+import java .util.List;
+public class ListCheck {
+    public static boolean onlyOddNumbers(List<Integer>list) {
+        for (int i : list) {
+            if (i % 2 == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(onlyOddNumbers(Arrays.asList(5,7,9,4)));
+    }
+}
