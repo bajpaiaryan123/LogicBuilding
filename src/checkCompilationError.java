82c2e0395cdbe6a608a516bff3c9479fe9e26a64
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-05-03 00:40:39 +0530

    My First Commit

---
+++ src/checkCompilationError.java
@@ -0,0 +1,8 @@
+public class checkCompilationError {
+    public static void main(String[] args) {
+        short a= 32767;
+        short b=1;
+        int sum =a+b;
+        System.out.println(sum);
+    }
+}
