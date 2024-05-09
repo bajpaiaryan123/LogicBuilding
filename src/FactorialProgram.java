2a5ec8f12c0a824e06f08c5b3ae436a390ed31b0
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-05-09 22:20:40 +0530

    My First Commit

---
+++ src/FactorialProgram.java
@@ -0,0 +1,12 @@
+public class FactorialProgram {
+    public static void main(String[] args) {
+        int i,fact=1;
+
+        for (i=1;i<=10;i++)
+        {
+            fact=fact*i;
+            System.out.println(fact);
+        }
+        System.out.println(fact);
+    }
+}
