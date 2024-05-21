2115282435d55c59b7713cad819c3fa0a4b53f16
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-05-21 20:56:04 +0530

    My First Commit

---
+++ src/PrintAsciiValue.java
@@ -0,0 +1,14 @@
+public class PrintAsciiValue {
+    public static void main(String[] args) {
+        char ch1='a';
+        char ch2 = 'b';
+
+        int asciiValue1 = ch1;
+        int asciiValue2 = ch2;
+
+        System.out.println("Ascii value of  "+ ch1 +" is : "+ asciiValue1);
+
+        System.out.println("Ascii value of  "+ ch2 +" is : "+ asciiValue2);
+
+    }
+}
