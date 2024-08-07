83ff4db695dd0e49efcc2b5a5c2701b6a6e33abe
Aryan Bajpai <bajpaiaryan221@gmail.com>
2024-08-07 06:50:19 +0530

    My First Commit

---
+++ src/PatternProgram07.java
@@ -0,0 +1,15 @@
+public class PatternProgram07 {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i=1; i<=n; i++){
+            for (int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
